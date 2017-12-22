138ceb07abaf723fe7e31f636baabf5f6a4e54d6
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-12-22 22:36:02 +0530

    first commit

---
+++ src/main/java/com/barath/webflux/app/SpringWebfluxFunctionalModelApplication.java
@@ -0,0 +1,12 @@
+package com.barath.webflux.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxFunctionalModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxFunctionalModelApplication.class, args);
+	}
+}
